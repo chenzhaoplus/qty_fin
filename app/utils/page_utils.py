4ac2f6e2dfcf92049668c0196ef23dcb496bd012
chenzhao <chenzhaoplus@163.com>
2021-08-09 18:45:04 +0800

    分页排序

--- app/utils/page_utils.py
+++ app/utils/page_utils.py
@@ -23,8 +23,6 @@ def get_page_sql(request, sql):
     form_json = request.json
     page_num = form_json['pageNum'] if form_json['pageNum'] else 1
     page_size = form_json['pageSize'] if form_json['pageSize'] else 10
-    # order = form_json['order'] if form_json['order'] else 'desc'
-    # sort = form_json['sort'] if form_json['sort'] else 'cast( `公司内在价值-净利润` AS DECIMAL(20,3) )'
     order = form_json.get('order') if form_json.get('order') else 'desc'
     sort = form_json.get('sort') if form_json.get('sort') else 'cast( `公司内在价值-净利润` AS DECIMAL(20,3) )'
     ret = f'''
